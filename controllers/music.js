529287ed61d140abb7f3e14b4223db2cba22fdf6
Rajaa <raja.aldaqaq@gmail.com>
2023-09-30 19:16:19 +0300

    add nav links and some style

--- controllers/music.js
+++ controllers/music.js
@@ -1,44 +1,41 @@
-const {Music} = require("../models/Music")
-const moment = require('moment');
+const { Music } = require('../models/Music')
+const moment = require('moment')
 
 exports.music_create_get = (req, res) => {
-  res.render("music/addmusic")
+  res.render('music/add')
 }
 
 exports.music_create_post = (req, res) => {
-
-  console.log("File Uploads - Audio Path: ", req.files.audio[0].path)
-  console.log("File Uploads - Image Path: ", req.files.image[0].path)
+  console.log('File Uploads - Audio Path: ', req.files.audio[0].path)
+  console.log('File Uploads - Image Path: ', req.files.image[0].path)
   // Create and Save a new Music document using Mongoose
-  const music = new Music( {
+  const music = new Music({
     name: req.body.name,
     author: req.body.author,
     description: req.body.description,
     lyrics: req.body.lyrics,
-    image: req.files.image[0].path, 
-    audio:  req.files.audio[0].path, 
+    image: req.files.image[0].path,
+    audio: req.files.audio[0].path
   })
 
-  music.save()
+  music
+    .save()
     .then(() => {
-      res.send("Music added successfully!")
+      res.send('Music added successfully!')
     })
     .catch((err) => {
-      console.error(err);
-      res.send("Please try again later.")
+      console.error(err)
+      res.send('Please try again later.')
     })
 }
-
 
 exports.music_showmusic_get = (req, res) => {
   Music.find()
     .then((musics) => {
       console.log(musics)
-      res.render("music/showmusic", { musics, moment });
+      res.render('music/index', { musics, moment })
     })
     .catch((err) => {
       console.log(err)
     })
 }
-
-
